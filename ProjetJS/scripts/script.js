2b695c83de5b2d0d76c61b83785610a3d10e5e3d
Thomas Fouquet <tho.fou@Dwwm.evx>
2024-11-04 13:02:46 +0000

    Ajout exercice 2 et 3 doc 6

--- ProjetJS/scripts/script.js
+++ ProjetJS/scripts/script.js
@@ -22,3 +22,49 @@
 // else {
 //     console.log(nombre + " est impaire")
 // }
+
+// Exercice 2 doc 6
+
+// let annee = prompt("Quel est votre année de naissance ?");
+// let age = 2024 - annee;
+// if (age<18) {
+//     console.log(age + " Vous êtes mineur")
+// }
+// else {
+//     console.log(age + " Vous êtes majeur")
+// }
+
+// Exercice 3 doc 6
+
+// Déclaration des variables
+// let num1 = parseInt(prompt("Entrez votre premier chiffre"));
+// let operateur = prompt("Entrez votre opération");
+// let num2 = parseInt(prompt("Entrez votre deuxième chiffre"));
+// let resultat;
+
+
+// Condition pour les différent opérateur
+// switch (operateur) {
+//   case "+":
+//     resultat = num1 + num2;
+//     break;
+
+//   case "-":
+//     resultat = num1 - num2;
+//     break;
+
+//   case "*":
+//     resultat = num1 * num2;
+//     break;
+
+//   case "/":
+//     // Si le 2ème numéro est 0, alors la division n'est pas possible
+//     if (num2 == 0) {
+//       resultat = "on ne peut pas diviser par zéro";
+//     } else {
+//       resultat = num1 / num2;
+//     }
+
+//     break;
+// }
+// alert(resultat);

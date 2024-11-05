ac17199d70159a9fdf36a3ff7a916d670bbe6e09
Thomas Fouquet <tho.fou@dwwm.evx>
2024-11-05 16:26:25 +0000

    Ajout exercice 8, 9 et 11 du doc 7

--- ProjetJS/scripts/script.js
+++ ProjetJS/scripts/script.js
@@ -7,31 +7,37 @@
 // console.log(prenom);
 // console.log(sexe);
 // if (sexe==true) {
-//     alert("Bonjour Monsieur " + nom + " " + prenom + "!\n Bienvenue sur notre site web !")
+//     alert("Bonjour Monsieur " + nom + " " + prenom + "!\n Bienvenue sur notre site web !");
 // }
 // else{
-//     alert("Bonjour Madame " + nom + " " + prenom + "!\n Bienvenue sur notre site web !")
+//     alert("Bonjour Madame " + nom + " " + prenom + "!\n Bienvenue sur notre site web !");
 // }
 
 // Exercice 1 doc 6
 
-// let nombre = prompt("Entrer un nombre");
-// if (nombre%2==0) {
-//     console.log(nombre + " est paire")
+// Déclaration de variable
+// let nombre = prompt("Entrer un nombre"); 
+
+// Si le reste de la division euclidienne est égale à 0 alors c'est paire sinon c'est impaire
+// if (nombre%2==0) { 
+//     console.log(nombre + " est paire");
 // }
 // else {
-//     console.log(nombre + " est impaire")
+//     console.log(nombre + " est impaire");
 // }
 
 // Exercice 2 doc 6
 
+// Déclaration de variable
 // let annee = prompt("Quel est votre année de naissance ?");
+
 // let age = 2024 - annee;
+// Si l'age est inferieur a 18 alors le user est mineur sinon, il est majeur.
 // if (age<18) {
-//     console.log(age + " Vous êtes mineur")
+//     console.log(age + " Vous êtes mineur");
 // }
 // else {
-//     console.log(age + " Vous êtes majeur")
+//     console.log(age + " Vous êtes majeur");
 // }
 
 // Exercice 3 doc 6
@@ -112,32 +118,37 @@
 
 // Exercice 5 doc 6
 
+// Déclaration de variable
 // let maried = confirm("Êtes vous marié ?");
 // let enfant = prompt("Combien avez-vous d'enfant ?");
 // let salaire = prompt("Quel est votre salaire ?");
 // let participation = 0;
 
+// Si à la question précedente il répond ok et ça renvoie true et il est marié sinon, il est celibataire.
 // if (maried == true) {
 //   participation += 25;
 // } else {
 //   participation += 20;
 // }
 
+// Tant que l'itération est inférieur au nombre d'enfant, alors on rajoute 10 à la participation.
 // for (let i = 0; i < enfant; i++) {
 //   participation += 10;
 // }
 
+// Si le salaire est inférieur à 1200 alors, on ajoute 10 à la participation.
 // if (salaire < 1200) {
 //   participation += 10;
 // }
 
-// if (participation >= 50) {
+// Si la participation est supérieur à 50 alors, la participation sera de maximum 50.
+// if (participation > 50) {
 //   participation = 50;
 // }
 
 // console.log("Vous avez le droit à " + participation + "% de participation.");
 
-// Exercice 1 doc 7
+// Exercice 1 doc 7 // Verifiez que tous les exos soit correcte et ajouter des commentaire.
 
 // let i = 1;
 // let prenom;
@@ -157,27 +168,26 @@
 //     console.log(i);
 // }
 
-// Exercice 3 doc 7 
+// Exercice 3 doc 7
 
 // let N = parseInt(prompt("Saisissez votre nombre."));
 // let sum = 0;
 
-// for (let i = N; i>=0; i--) {
-//     sum += i
+// for (let i = N; i > 0; i--) {
+//   sum += i;
+//   console.log(sum);
 // }
-
-// console.log(sum);
 
 // Exercice 4 doc 7
 
 // let n1 = parseInt(prompt("Veuillez saisir le premier numero"));
 // let n2 = parseInt(prompt("Veuillez saisir le deuxième numero"));
-// let sum = 0
+// let sum = 0;
 
 // Calcul des intervalles de n1 à n2
 // for (let i = n1; i<=n2; i++) {
-//     sum+=i
-//     console.log(sum)
+//     sum+=i;
+//     console.log(sum);
 // }
 
 // Exercice 5 doc 7
@@ -204,7 +214,7 @@
 // console.log(nbCount);
 // console.log(nbMoyenne);
 
-// Exercice 6 doc 7 
+// Exercice 6 doc 7
 
 // let nbSomme = 0;
 // let nbCount = 0;
@@ -224,11 +234,11 @@
 //     nbCount++;
 
 //     if (nb<nbMin) {
-//         nbMin = nb
+//         nbMin = nb;
 //     }
 
 //     else if (nb>nbMax) {
-//         nbMax = nb
+//         nbMax = nb;
 //     }
 // }
 
@@ -249,3 +259,65 @@
 //     let NX = i *X;
 //     console.log(i + " x " + X + " = " + NX);
 // }
+
+// Exercice 8 doc 7
+
+// let mot = prompt("Saisir un mot.");
+// let voyelles = "a,e,i,o,u,y,A,E,I,O,U,Y";
+// let voyellesCount = 0;
+
+// for (let i =0; i < mot.length; i++) {
+//     let index = mot.substring(i, i + 1);
+//     if (voyelles.indexOf(index) !== -1) {
+//         voyellesCount++;
+//     }
+
+// }
+
+// console.log(mot);
+// console.log(voyellesCount);
+
+// Exercice 9 doc 7
+
+// let jeunes = 0; // Jeunes < 20ans
+// let moyen = 0; // Moyens >= 20ans <= 40 ans
+// let seniors = 0; // Seniors > 40ans
+// let age = 0;
+
+// while(age < 100) {
+//     age = prompt("Saisir votre age");
+//     if (age < 20) {
+//         jeunes++;
+//     }
+//     else if (age >= 20 && age <= 40) {
+//         moyen ++;
+//     }
+//     else {
+//         seniors++;
+//     }
+//     console.log(age);
+// }
+
+// console.log("Il y a " + jeunes + " jeunes.");
+// console.log("Il y a " + moyen + " moyens.");
+
+// Exercice 11 doc 7
+// let magic = parseInt(Math.floor((Math.random() * 100) + 1));
+// let user = parseInt(prompt("Veuillez saisir un nombre entre 0 et 100"));
+
+// while (user != magic) {
+
+//     if (user == magic) {
+//         alert("Vous avez gagner !");
+//         break;
+//     }
+
+//     else if (user < magic) {
+//         user = prompt("Veuillez saisir un nombre plus grand.");
+//     }
+
+//     else {
+//         user = prompt("Veuillez saisir un nombre plus petit");
+//     }
+// }
+// console.log(magic);

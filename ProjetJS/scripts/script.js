a7381fa17662a41dc404c4fe8dfdaf8a0c5a7c8b
Thomas <thomasfouquet8@gmail.com>
2024-11-06 16:48:18 +0000

    Ajout exercice 1 et 2 du doc 8

--- ProjetJS/scripts/script.js
+++ ProjetJS/scripts/script.js
@@ -425,8 +425,31 @@
 // maFonction2();
 // console.log("mafonction2 > plop2 : "+plop2);
 
-// Exercice 1 doc 7 
+// Exercice 1 doc 8 
 
+// Fonction qui retourne le produit de x, y.
 // function produit(x,y) {
-
+//     return x*y;
 // }
+
+// n1 = parseInt(prompt("Veuillez saisir un nombre."));
+// n2 = parseInt(prompt("Veuillez saisir un nombre."));
+
+// document.write("<p>Le produit de "+ n1 +" x "+ n2 +" est égal à "+ produit(n1,n2) +".</p>");
+
+// // Fonction qui affiche une image.
+// function afficheImg(image) {
+//     return image;
+// }
+
+// lien =prompt("Veuillez saisir le lien de l'image que vous souhaiter afficher.");
+// document.write("<img src="+afficheImg(lien)+">");
+
+// Exercice 2 doc 8
+
+function TableMultiplication(nb){
+    let i = 0;
+    while (i<= 10) {
+        i * nb;
+    }
+} 

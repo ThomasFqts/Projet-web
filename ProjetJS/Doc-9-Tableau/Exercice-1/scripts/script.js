ecea3871585de826c52669779dd215aaf50301c4
Thomas <thomasfouquet8@gmail.com>
2024-11-07 22:32:07 +0100

    Ajout exercice complet des tableau

--- ProjetJS/Doc-9-Tableau/Exercice-1/scripts/script.js
+++ ProjetJS/Doc-9-Tableau/Exercice-1/scripts/script.js
@@ -1,1 +1,9 @@
 // Exercice 1
+
+// let taille = parseInt(prompt("Veuillez saisir la taille de votrre tableau"));
+// let tableau = [];
+// for (let i = 0; i<taille;i++) {
+//     let valeurs = prompt("Veuillez saisir votre valeur");
+//     tableau.push(valeurs);
+// }
+// console.log("Voici votre tableau : " + tableau);

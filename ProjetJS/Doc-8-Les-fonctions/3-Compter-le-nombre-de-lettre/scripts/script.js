cab90b6773c5104b661e1147a481259ed47160c7
Thomas <thomasfouquet8@gmail.com>
2024-11-07 11:32:37 +0100

    Exercice 3 fini

--- ProjetJS/Doc-8-Les-fonctions/3-Compter-le-nombre-de-lettre/scripts/script.js
+++ ProjetJS/Doc-8-Les-fonctions/3-Compter-le-nombre-de-lettre/scripts/script.js
@@ -1,1 +1,31 @@
 // Exercice 3
+
+// function Count (phrase,lettre) {
+//     if (lettre.length !== 1 || !/^[a-zA-Z]$/.test(lettre)) {
+//         throw new Error("Le paramètre 'lettre' doit être un seul caractère.");
+//     }
+//     let Count = 0;
+//     for (let i = 0; i<phrase.length; i++) {
+//         let index = phrase.substring(i, i+1);
+//         if (lettre.indexOf(index) !== -1) {
+//             Count ++;
+//         }
+//     }
+//     return Count;
+// }
+
+// ltre = prompt("Veuillez saisir votre lettre.");
+// phrse = prompt("Veuillez saisir votre phrase.");
+// console.log(Count(phrse,ltre));
+
+// function compterLettre(phrase, lettre) {
+//     let count = 0; // Initialise le compteur à 0
+
+//     for (let i = 0; i < phrase.length; i++) {
+//         if (phrase[i] === lettre) { // Vérifie si le caractère courant est égal à "lettre"
+//             count++; // Incrémente le compteur si on trouve une correspondance
+//         }
+//     }
+
+//     return count; // Retourne le nombre total de fois où "lettre" apparaît dans "phrase"
+// }

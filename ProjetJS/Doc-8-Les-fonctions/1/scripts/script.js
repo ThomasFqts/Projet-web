66d1db654cd44aae7ddc5c07e99c5ad28cac36ba
Thomas <thomasfouquet8@gmail.com>
2024-11-07 09:35:26 +0100

    Suppresion de l'exercice exemple dans l'exercice 1 des fonctions

--- ProjetJS/Doc-8-Les-fonctions/1/scripts/script.js
+++ ProjetJS/Doc-8-Les-fonctions/1/scripts/script.js
@@ -1,21 +1,3 @@
-// Exercice exemple doc 8
-
-// function maFonction()
-// {
-// let plop1 = 123;
-// return plop1;
-// }
-// function maFonction2()
-// {
-// plop2 = 456;
-// }
-// plop1 = maFonction();
-
-// console.log("fonction 1 / plop1 : "+plop1);
-
-// maFonction2();
-// console.log("mafonction2 > plop2 : "+plop2);
-
 // Exercice 1 doc 8 
 
 // Fonction qui retourne le produit de x, y.
